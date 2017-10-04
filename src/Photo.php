1850706e6e1b785834c48d6b13363c6a57fba9b7
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-10-04 15:12:33 -0300

    M  src/Photo.php

--- src/Photo.php
+++ src/Photo.php
@@ -3,8 +3,22 @@ namespace Basic;
 //https://github.com/MeuGarfo/SimpleImage
 class Photo{
     var $image;
-    function size(){
-
+    function auto_orient($src,$dst){
+    	//autoOrient()
+    }
+    function crop($src,$dst,$x1,$y1,$x2,$y2){
+    	//crop($x1, $y1, $x2, $y2)
+    }
+    function info($src){
+    	//getMimeType()
+    	//getHeight()
+    	//getWidth()
+    }
+    function resize($src,$dst,$max_width,$max_height){
+    	//bestFit($maxWidth, $maxHeight)
+    }
+    function thumbnail($src,$dst,$width,$height){
+        //thumbnail($width, $height, $anchor)
     }
 }
 ?>
